92f6809c1bf958f95e62bef6be2b6ba22fa9c833
201216323 <201216323qwertyuiop>
2016-10-11 18:56:08 +0800

    头布局超出手机屏幕时候滑动，增加活动中嵌套碎片页面，于自己项目中的效果达成一致。需要注意的是：headview的id,TabLayout（或者PagerSlidingTabStrip）的id，viewpager的id不能修改。

--- app/src/main/java/com/bing/sticklayout/MainActivity.java
+++ app/src/main/java/com/bing/sticklayout/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends FragmentActivity {
 
     private FrameLayout frameLayout;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
